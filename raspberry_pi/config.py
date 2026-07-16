c35e2518b926aa41ee57f95de5eba02600872244
ebiten <ceti1240164@gn.iwasaki.ac.jp>
2026-07-16 10:46:21 +0900

    Update raspberry pi camera support

--- raspberry_pi/config.py
+++ raspberry_pi/config.py
@@ -1,18 +1,20 @@
 # PresentationEscort Raspberry Pi Config
 
-# スマホIPカメラを使う
-USE_IP_CAMERA = True
+# 使用するカメラ方式
+# "ip"  : スマホIPカメラ
+# "usb" : USB Webカメラ
+CAMERA_MODE = "usb"
 
-# スマホIPカメラアプリに表示されたURLに変更する
-IP_CAMERA_URL = "http://192.168.x.x:8080/video"
+# スマホIPカメラ用URL
+# 例: "http://192.168.1.10:8080/video"
+IP_CAMERA_URL = "http://10.17.9.38:8081"
 
-# USBカメラ用。今回は使わない
+# USB Webカメラ用
 CAMERA_INDEX = 0
 
+# 取得したい解像度
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
-CENTER_X = FRAME_WIDTH // 2
-CENTER_Y = FRAME_HEIGHT // 2
-
+# デバッグ表示
 SHOW_WINDOW = True
